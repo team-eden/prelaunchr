2bce00aa73db0c89cdc57faa293ec2b2fab86c6d
Ryan Yockey <ryan@habit.com>
2017-11-14 11:28:37 -0800

    setup for nanobox

--- config/boot.rb
+++ config/boot.rb
@@ -4,3 +4,13 @@ require 'rubygems'
 ENV['BUNDLE_GEMFILE'] ||= File.expand_path('../../Gemfile', __FILE__)
 
 require 'bundler/setup' if File.exists?(ENV['BUNDLE_GEMFILE'])
+
+require 'rails/commands/server'
+module Rails
+  class Server
+    alias :_default_options :default_options
+    def default_options
+      _default_options.merge!(Host:'0.0.0.0')
+    end
+  end
+end
