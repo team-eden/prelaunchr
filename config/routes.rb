b5fac884e71383ad632694ee11cffaf19d731cef
Sterling Cobb <sterlingcobb@gmail.com>
2017-11-16 20:27:05 -0800

    fix issues with t&c

--- config/routes.rb
+++ config/routes.rb
@@ -11,6 +11,7 @@ Prelaunchr::Application.routes.draw do
   post 'users/create' => 'users#create'
   get 'refer-a-friend' => 'users#refer'
   get 'privacy-policy' => 'users#policy'
+  get 'terms' => 'pages#terms'
 
   unless Rails.application.config.consider_all_requests_local
     get '*not_found', to: 'users#redirect', :format => false
